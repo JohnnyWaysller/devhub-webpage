af883794fb4c4559d0724e697efd2e3c32d91d59
aulagit <aulagit@example.com>
2025-10-10 20:05:45 -0300

    feat: Correção botoes/botao dropdown e adicao do footer.

--- src/main.js
+++ src/main.js
@@ -17,6 +17,7 @@ document.getElementById('switch-btn').addEventListener('click', () => {
 
 function themeSwitch() {
     const rootStyles = window.getComputedStyle(root); 
+    const navbar = document.getElementById('main-navbar')
 
     if (themeUse === 'dark') {
         root.style.setProperty('--bg-color', rootStyles.getPropertyValue('--light-container-value').trim());
@@ -27,6 +28,10 @@ function themeSwitch() {
         root.style.setProperty('--text-theme', rootStyles.getPropertyValue('--dark-bg-value').trim());
 
         root.style.setProperty('--placeholder-color', rootStyles.getPropertyValue('--light-placeholder-value').trim());
+        
+        navbar.classList.remove('navbar-dark');
+        navbar.classList.add('navbar-light');
+
         themeUse = 'light';
     } else {
         root.style.setProperty('--bg-color', rootStyles.getPropertyValue('--dark-bg-value').trim());
@@ -36,6 +41,10 @@ function themeSwitch() {
         root.style.setProperty('--text-theme', rootStyles.getPropertyValue('--light-bg-value').trim());
 
         root.style.setProperty('--placeholder-color', rootStyles.getPropertyValue('--dark-placeholder-value').trim());
+
+        navbar.classList.remove('navbar-light');
+        navbar.classList.add('navbar-dark');
+
         themeUse = 'dark';
 
     }
